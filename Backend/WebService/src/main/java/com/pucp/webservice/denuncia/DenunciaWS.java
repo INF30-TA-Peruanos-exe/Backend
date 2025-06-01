08beb5b56e1eb7e4dc71df695012d67d4a29be0a
marioSaico <mssopro2005@gmail.com>
2025-06-01 15:46:18 -0500

    DENUNCIA WS COMPLETO

--- Backend/WebService/src/main/java/com/pucp/webservice/denuncia/DenunciaWS.java
+++ Backend/WebService/src/main/java/com/pucp/webservice/denuncia/DenunciaWS.java
@@ -4,22 +4,70 @@
  */
 package com.pucp.webservice.denuncia;
 
+import com.pucp.capadominio.denuncia.Denuncia;
+import com.pucp.capanegocio.denuncias.DenunciaServiceImpl;
+import com.pucp.capanegocio.interfacesService.DenunciaService;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
 import jakarta.jws.WebService;
+import jakarta.xml.ws.WebServiceException;
+import java.util.ArrayList;
 
 /**
  *
  * @author Axel
  */
-@WebService(serviceName = "DenunciaWS")
+@WebService(serviceName = "DenunciaWS", targetNamespace = "com.pucp.pucpqhatu")
 public class DenunciaWS {
 
-    /**
-     * This is a sample web service operation
-     */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    private final DenunciaService denunciaService;
+    
+    public DenunciaWS(){
+        denunciaService = new DenunciaServiceImpl();
+    }
+    
+    @WebMethod(operationName = "registrarDenuncia")
+    public void registrarDenuncia(@WebParam(name = "denuncia") Denuncia denuncia){
+        try {
+            denunciaService.registrarDenuncia(denuncia);
+        } catch (Exception ex) {
+            throw new WebServiceException("Error al registrar denuncia: " + ex.getMessage());
+        }
+    }
+    
+    @WebMethod(operationName = "actualizarDenuncia")
+    public void actualizarDenuncia(@WebParam(name = "denuncia") Denuncia denuncia){
+        try {
+            denunciaService.actualizarDenuncia(denuncia);
+        } catch (Exception ex) {
+            throw new WebServiceException("Error al actualizar facultad: " + ex.getMessage());
+        }
+    }
+    
+    @WebMethod(operationName = "eliminarDenuncia")
+    public void eliminarDenuncia(@WebParam(name = "idDenuncia") int idDenuncia){
+        try {
+            denunciaService.eliminarDenuncia(idDenuncia);
+        } catch (Exception ex) {
+            throw new WebServiceException("Error al eliminar denuncia: " + ex.getMessage());
+        }
+    }
+    
+    @WebMethod(operationName = "obtenerDenuncia")
+    public Denuncia obtenerDenuncia(@WebParam(name = "idDenuncia") int idDenuncia){
+        try {
+            return denunciaService.obtenerDenuncia(idDenuncia);
+        } catch (Exception ex) {
+            throw new WebServiceException("Error al obtener denuncia: " + ex.getMessage());
+        }        
+    }
+    
+    @WebMethod(operationName = "listarDenuncia")
+    public ArrayList<Denuncia> listarDenuncia(){
+        try {
+            return denunciaService.listarDenuncia();
+        } catch (Exception ex) {
+            throw new WebServiceException("Error al listar denuncias: " + ex.getMessage());
+        }        
     }
 }
