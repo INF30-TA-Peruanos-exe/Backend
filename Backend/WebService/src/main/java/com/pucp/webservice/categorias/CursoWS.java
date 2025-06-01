bdc6cd9b8c08abb818ae3eff34a6f5294fd07735
CardenasAxel <cardenas.axel@pucp.edu.pe>
2025-06-01 12:02:47 -0500

    Corregido el WebService

---
+++ Backend/WebService/src/main/java/com/pucp/webservice/categorias/CursoWS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/WebService.java to edit this template
+ */
+package com.pucp.webservice.categorias;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import jakarta.jws.WebService;
+
+/**
+ *
+ * @author Axel
+ */
+@WebService(serviceName = "CursoWS")
+public class CursoWS {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
