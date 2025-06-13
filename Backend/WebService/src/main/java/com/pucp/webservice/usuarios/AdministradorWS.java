751f1b7427171830d9468c4b8a42ed2ad4c2dcd7
marioSaico <mssopro2005@gmail.com>
2025-06-13 11:03:44 -0500

    ADMINISTRADOR WS COMPLETO

--- Backend/WebService/src/main/java/com/pucp/webservice/usuarios/AdministradorWS.java
+++ Backend/WebService/src/main/java/com/pucp/webservice/usuarios/AdministradorWS.java
@@ -4,22 +4,72 @@
  */
 package com.pucp.webservice.usuarios;
 
+import com.pucp.capadominio.usuarios.Administrador;
+import com.pucp.capanegocio.interfacesService.AdministradorService;
+import com.pucp.capanegocio.usuarios.AdministradorServiceImpl;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
 import jakarta.jws.WebService;
+import jakarta.xml.ws.WebServiceException;
+import java.util.ArrayList;
 
 /**
  *
  * @author Axel
  */
-@WebService(serviceName = "AdministradorWS")
+@WebService(serviceName = "AdministradorWS", targetNamespace = "com.pucp.pucpqhatu")
 public class AdministradorWS {
 
-    /**
-     * This is a sample web service operation
-     */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    private final AdministradorService administradorService;
+    
+    public AdministradorWS(){
+        administradorService = new AdministradorServiceImpl();
     }
+    
+    @WebMethod(operationName = "registrarAdministrador")
+    public void registrarAdministrador(@WebParam(name = "administrador") Administrador administrador){
+        try {
+            administradorService.registrarAdministrador(administrador);
+        } catch (Exception ex) {
+            throw new WebServiceException("Error al registrar administrador: " + ex.getMessage());
+        }       
+    }
+    
+    @WebMethod(operationName = "actualizarAdministrador")
+    public void actualizarAdministrador(@WebParam(name = "usuario") Administrador administrador) {
+        try {
+            administradorService.actualizarAdministrador(administrador);
+        } catch (Exception ex) {
+            throw new WebServiceException("Error al actualizar administrador: " + ex.getMessage());
+        }
+    }
+    
+    @WebMethod(operationName = "eliminarAdministrador")
+    public void eliminarAdministrador(@WebParam(name = "idAdministrador") int idAdministrador) {
+        try {
+            administradorService.eliminarAdministrador(idAdministrador);
+        } catch (Exception ex) {
+            throw new WebServiceException("Error al eliminar administrador: " + ex.getMessage());
+        }
+    }
+    
+    @WebMethod(operationName = "obtenerAdministrador")
+    public Administrador obtenerAdministrador(@WebParam(name = "idAdministrador") int idAdministrador) {
+        try {
+            return administradorService.obtenerAdministrador(idAdministrador);
+        } catch (Exception ex) {
+            throw new WebServiceException("Error al obtener administrador: " + ex.getMessage());
+        }
+    }
+    
+    @WebMethod(operationName = "listarAdministrador")
+    public ArrayList<Administrador> listarAdministrador() {
+        try {
+            return administradorService.listarAdministrador();
+        } catch (Exception ex) {
+            throw new WebServiceException("Error al listar administradores: " + ex.getMessage());
+        }
+    }
+    
+    
 }
