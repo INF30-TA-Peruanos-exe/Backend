6ce28e26981a7267bcdfc18d9a82c9871a978ddb
marioSaico <mssopro2005@gmail.com>
2025-06-28 19:43:42 -0500

    POOL DE CONEXIONES

--- Backend/CapaPersistencia/src/main/java/com/pucp/config/DBManager.java
+++ Backend/CapaPersistencia/src/main/java/com/pucp/config/DBManager.java
@@ -1,18 +1,21 @@
 package com.pucp.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
+//import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class DBManager {
     private static DBManager instance;
-
-    private String jdbcUrl;
-    private String username;
-    private String password;
+    private HikariDataSource dataSource;
+    
+//    private String jdbcUrl;
+//    private String username;
+//    private String password;
 
     private DBManager() {
         cargarConfiguracion();
@@ -35,23 +38,46 @@ public class DBManager {
             }
 
             properties.load(input);
-
+            
+            HikariConfig config = new HikariConfig();
             String dbType = properties.getProperty("db.type");
-            this.jdbcUrl = properties.getProperty(dbType + ".jdbcUrl");
-            this.username = properties.getProperty(dbType + ".username");
-            this.password = properties.getProperty(dbType + ".password");
+            config.setJdbcUrl(properties.getProperty(dbType + ".jdbcUrl"));
+            config.setUsername(properties.getProperty(dbType + ".username"));
+            config.setPassword(properties.getProperty(dbType + ".password"));
 
+            // Configuración del pool
+            config.setMaximumPoolSize(Integer.parseInt(properties.getProperty("db.maxPoolSize")));
+            config.setMinimumIdle(Integer.parseInt(properties.getProperty("db.minIdle")));  
+            config.setIdleTimeout(Integer.parseInt(properties.getProperty("db.idleTimeout")));
+            config.setConnectionTimeout(Integer.parseInt(properties.getProperty("db.connectionTimeout")));
+//            this.jdbcUrl = properties.getProperty(dbType + ".jdbcUrl");
+//            this.username = properties.getProperty(dbType + ".username");
+//            this.password = properties.getProperty(dbType + ".password");
+                    // Configuraciones específicas según el tipo de base de datos
+            if ("mysql".equals(dbType)) {
+                config.addDataSourceProperty("cachePrepStmts", "true");
+                config.addDataSourceProperty("prepStmtCacheSize", "250");
+                config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+            }
+            config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            dataSource = new HikariDataSource(config);
         } catch (IOException e) {
             throw new RuntimeException("Error al cargar configuración de base de datos", e);
         }
     }
 
-    public Connection obtenerConexion() throws SQLException {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(jdbcUrl, username, password);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Driver JDBC no encontrado", e);
+    public Connection obtenerConexion() throws SQLException { 
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            return DriverManager.getConnection(jdbcUrl, username, password);
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException("Driver JDBC no encontrado", e);
+//        }
+        return dataSource.getConnection();
+    }
+    public void cerrarPool() {
+        if (dataSource != null) {
+            dataSource.close();
         }
     }
 }
